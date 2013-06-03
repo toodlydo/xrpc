f2a2b92a1f8d3d9fbdecdc2441771a3fa0db0119
toodlydo <toodlydo@hotmail.com>
2013-06-03 15:16:14 -0500

    added xbmc authentication to settings file

--- settings.php
+++ settings.php
@@ -2,6 +2,17 @@
 /*
 	Settings File
 */
-	$url = "http://localhost:81/jsonrpc"; //path to the xbmc server
+	$XBMC_USER = "xbmc";
+	$XBMC_PASS = "xbmc";
+	$XBMC_IP = "localhost";
+	$XBMC_PORT = "81";
+	$url = "";
+
+	if($XBMC_PASS == ""){
+		$url = "http://" . $XBMC_IP . ":" . $XBMC_PORT . "/jsonrpc";
+	} else{
+		$url = "http://" . $XBMC_USER . ":" . $XBMC_PASS . "@" . $XBMC_IP . ":" . $XBMC_PORT . "/jsonrpc";
+	}
+
 	$request = '{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "id": 1, "params":{"properties":["art","genre","plot","title","originaltitle","year","rating","thumbnail","playcount","file","fanart","episode"]}}';
 ?>
